d0158b7f944def0f8eda2c882cc7c24c4cc7bee4
Andy Moore <moorejandy@gmail.com>
2020-02-02 12:59:12 -0500

    trying to add values to tables with associations.

--- models/qandanswer.js
+++ models/qandanswer.js
@@ -1,13 +1,32 @@
+var Sequelize = require("sequelize");
+
+var connection = new Sequelize("Trump_db", "root", "", {
+  host: "localhost",
+  dialect: "mysql"
+});
 module.exports = function(sequelize, DataTypes) {
   var Qanswers = sequelize.define("Qanswers", {
-    question: DataTypes.STRING,
-    ansOne: DataTypes.STRING,
-    anstwo: DataTypes.STRING,
-    ansThree: DataTypes.STRING,
-    ansFour: DataTypes.STRING,
-    correct: DataTypes.STRING
+    question: { type: DataTypes.STRING },
+    ansOne: { type: DataTypes.STRING },
+    anstwo: { type: DataTypes.STRING },
+    ansThree: { type: DataTypes.STRING },
+    ansFour: { type: DataTypes.STRING },
+    correct: { type: DataTypes.STRING }
   });
-
+  connection
+    .sync({
+      force: true
+    })
+    .then(function() {
+      Tweets.create({
+        question: "Fill in the blank",
+        ansOne: "Hillary Clinton",
+        anstwo: "Bette Midler",
+        ansThree: "Megyn Kelly",
+        ansFour: "Betty White",
+        correct: "Bette Midler"
+      });
+    });
   Qanswers.associate = function(models) {
     // We're saying that a Post should belong to an Author
     // A Post can't be created without an Author due to the foreign key constraint
