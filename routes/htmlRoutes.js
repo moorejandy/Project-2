2ef11ddae5045cea0a2772db46e60aaaeaa3ec9a
Samantha Gaiser <sgaiser3@gmail.com>
2020-02-06 16:17:58 -0500

    Added scss for the transition from landing page to game page. Co-authored-by: Kembly <michael@kembly.com>

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,13 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Tweets.findAll({}).then(function(foundTweets) {
+    res.render("home");
+  });
+
+  app.get("/all-tweets", function(req, res) {
+    db.Tweets.findAll({ raw: true }).then(function(foundTweets) {
+      // Before { raw: true } => data looked like this [{ dataValues: { id: 1, tweet: 'someTweet' } }],
+      // After { raw: true } => data looks like this [{ id: 1, tweet: 'someTweet' }]
       console.log("FOUND TWEETS!, ", foundTweets);
       // You want to send an array of the tweet strings back rather than an array of objects
       res.render("index", {
@@ -14,8 +20,11 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
+  // Change Example to Tweets so you're getting data from Tweets table
+  // Make sure you are getting JS objects by using { raw: true }
+  // rename the variables to reference tweet instead of example so dbExample => dbTweet // this is just a convention thing, it will still work without renaming
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+    db.Example.findOne({ where: { id: req.params.id }, raw: true }).then(function(
       dbExample
     ) {
       res.render("example", {
